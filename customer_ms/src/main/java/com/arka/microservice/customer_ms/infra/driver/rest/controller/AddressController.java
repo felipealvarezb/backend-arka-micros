4639e45cad952cd3003ed349fea06431fa99f1b3
Felipe Alvarez <felipe.alvarez@genteintegral.com>
2025-06-17 12:51:51 -0500

    feat(swagger): adding swagger micro-user

--- customer_ms/src/main/java/com/arka/microservice/customer_ms/infra/driver/rest/controller/AddressController.java
+++ customer_ms/src/main/java/com/arka/microservice/customer_ms/infra/driver/rest/controller/AddressController.java
@@ -4,6 +4,8 @@ import com.arka.microservice.customer_ms.domain.ports.in.IAddressInPort;
 import com.arka.microservice.customer_ms.infra.driver.rest.dto.request.AddressRequest;
 import com.arka.microservice.customer_ms.infra.driver.rest.dto.response.AddressResponseDTO;
 import com.arka.microservice.customer_ms.infra.driver.rest.mapper.IAddressRestMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +15,13 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/address")
 @RequiredArgsConstructor
+@Tag(name = "Address", description = "Gestión de direcciones")
 public class AddressController {
 
   private final IAddressInPort addressInPort;
   private final IAddressRestMapper addressRestMapper;
 
+  @Operation(summary = "Add Address", description = "Create and save an address")
   @PostMapping("/add")
   @ResponseStatus(HttpStatus.CREATED)
   public Mono<AddressResponseDTO> addAddress(@RequestBody AddressRequest request) {
@@ -25,6 +29,7 @@ public class AddressController {
             .map(addressRestMapper::modelToAddressResponseDTO);
   }
 
+  @Operation(summary = "Update Address", description = "Update and save an address")
   @PutMapping("/update/{id}")
   @ResponseStatus(HttpStatus.OK)
   public Mono<AddressResponseDTO> updateAddress(@PathVariable Long id, @RequestBody AddressRequest request) {
@@ -32,6 +37,7 @@ public class AddressController {
             .map(addressRestMapper::modelToAddressResponseDTO);
   }
 
+  @Operation(summary = "List Address", description = "List all user address")
   @GetMapping("/list")
   @ResponseStatus(HttpStatus.OK)
   public Flux<AddressResponseDTO> getAddresses() {
@@ -39,6 +45,7 @@ public class AddressController {
             .map(addressRestMapper::modelToAddressResponseDTO);
   }
 
+  @Operation(summary = "Delete Address", description = "Delete an address")
   @DeleteMapping("/delete/{id}")
   @ResponseStatus(HttpStatus.OK)
   public Mono<String> deleteAddress(@PathVariable Long id) {
--- customer_ms/src/main/java/com/arka/microservice/customer_ms/infra/driver/rest/controller/AdminController.java
+++ customer_ms/src/main/java/com/arka/microservice/customer_ms/infra/driver/rest/controller/AdminController.java
@@ -6,6 +6,8 @@ import com.arka.microservice.customer_ms.infra.driver.rest.dto.request.RegisterU
 import com.arka.microservice.customer_ms.infra.driver.rest.dto.response.UserProfileDTO;
 import com.arka.microservice.customer_ms.infra.driver.rest.dto.response.UserWebClientDTO;
 import com.arka.microservice.customer_ms.infra.driver.rest.mapper.IUserRestMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,23 +19,27 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@Tag(name = "Admin", description = "Gestión de administradores")
 public class AdminController {
 
   private final IUserInPort userInPort;
   private final IUserRestMapper userMapper;
 
+  @Operation(summary = "Register admin", description = "Create and save an Admin")
   @PostMapping("/register")
   @ResponseStatus(HttpStatus.CREATED)
   public Mono<UserModel> registerAdmin(@RequestBody RegisterUserRequest request) {
     return userInPort.registerAdmin(userMapper.dtoToModel(request));
   }
 
+  @Operation(summary = "Register admin logistic", description = "Create and save an Admin logistic")
   @PostMapping("/register/admin-logistic")
   @ResponseStatus(HttpStatus.CREATED)
   public Mono<UserModel> registerAdminLogistic(@RequestBody RegisterUserRequest request) {
     return userInPort.registerAdminLogistic(userMapper.dtoToModel(request));
   }
 
+  @Operation(summary = "Get user list", description = "Get all users with filters")
   @GetMapping("/user-list")
   @ResponseStatus(HttpStatus.OK)
   public Flux<UserProfileDTO> getUserList(@RequestParam Optional<String> email,
@@ -43,6 +49,7 @@ public class AdminController {
     return userInPort.listAllUsers(email, dni, name, page).map(userMapper::modelToUserProfileDTO);
   }
 
+  @Operation(summary = "Get Admin Logistic", description = "Get an Admin Logistic")
   @GetMapping("/admin-logistic/{id}")
   @ResponseStatus(HttpStatus.OK)
   public Mono<UserWebClientDTO> getAdminLogistic(@PathVariable Long id) {
